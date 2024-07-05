ad7785c7e20ebac4a8984d3f06a6694c8b9a17fa
Rimuru <Rimuru.Dev@gmail.com>
2024-07-06 01:20:06 +0400

    Update SceneAutoInjector.cs

--- IndieInject/Assets/Plugins/IndieInject/Usage/SceneAutoInjector.cs
+++ IndieInject/Assets/Plugins/IndieInject/Usage/SceneAutoInjector.cs
@@ -20,7 +20,7 @@ namespace IndieInject
 {
     public sealed class SceneAutoInjector : MonoBehaviour
     {
-        private void Start()
+        private void Awake()
         {
             var all = FindObjectsOfType<MonoBehaviour>(true);
 
